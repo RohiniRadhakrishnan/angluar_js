e66ede867c78bc3808a7bf4957d350d20f34d486
Nagarohini R <naga3009@global.chainsys.com>
2022-03-28 10:20:48 +0530

    currency

--- src/jslessons/scripts/default.js
+++
@@ -1,8 +0,0 @@
-//default value fot he parameter
-function yell(name = 'Silly Goose') {
-  return `HEY ${name.toUpperCase()}`;
-}
-let greetings = yell('rohini');
-console.log(greetings);
-greetings = yell();
-console.log(greetings);
